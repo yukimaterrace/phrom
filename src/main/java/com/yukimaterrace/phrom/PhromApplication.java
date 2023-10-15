e45c74a9755f34a9738bc9b91ab81d34cfb0e24c
Yuki Matsuo <yukimaterrace@gmail.com>
2023-10-15 17:31:59 +0900

    initial commit

---
+++ src/main/java/com/yukimaterrace/phrom/PhromApplication.java
@@ -0,0 +1,13 @@
+package com.yukimaterrace.phrom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhromApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhromApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yukimaterrace/phrom/PhromApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yukimaterrace.phrom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhromApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
